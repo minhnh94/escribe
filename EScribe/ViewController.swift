f1fc569274ec29a34276f03f5d067b31117b8c75
Minh Hoang <minhnh.da@gmail.com>
2017-02-22 17:25:13 +0700

    init

---
+++ EScribe/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EScribe
+//
+//  Created by minhnh on 2/14/17.
+//
+//
+
+import UIKit
+import SpeechKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
