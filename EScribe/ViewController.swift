1af5f702715876e74f544d9020e73c7d3e7c6368
Minh Hoang <minhnh.da@gmail.com>
2017-02-26 23:49:08 +0700

    basic speech to text and basic fill form

--- EScribe/ViewController.swift
+++ EScribe/ViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 import SpeechKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate, SKTransactionDelegate {
 
+    // MARK: - API related data
+    let API_URL = "nmsps://NMDPTRIAL_minhnh_da_gmail_com20170206055537@sslsandbox-nmdp.nuancemobility.net:443"
+    let SSL_HOST = "sslsandbox-nmdp.nuancemobility.net"
+    let SSL_PORT = "443"
+    let APP_ID = "NMDPTRIAL_minhnh_da_gmail_com20170206055537"
+    let APP_KEY = "9b35dcfe5ce50ab03bd366d929f2c775cedc1b88ab44b13340625481bc104357461f55a20460534033b9195cabe1d17abdec026930b39c7115f0f2d101352847"
+    var SERVER_URL: String!
+    
+    // MARK: - Controller properties
+    var skSession: SKSession?
+    var skTransaction: SKTransaction?
+    var currentProcessingText: UIView?
+    
+    // MARK: - Controller functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        SERVER_URL = String(format: "nmsps://%@@%@:%@", APP_ID, SSL_HOST, SSL_PORT)
+        skSession = SKSession(url: URL(string: SERVER_URL), appToken: APP_KEY)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +38,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Text field delegates
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        voiceRecognitionTransactionStarted()
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        voiceRecognitionTransactionStarted()
+    }
+    
+    func voiceRecognitionTransactionStarted() {
+        skTransaction = skSession!.recognize(withType: SKTransactionSpeechTypeDictation, detection: .none, language: "eng-USA", options: nil, delegate: self)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        skTransaction?.stopRecording()
+        currentProcessingText = textView
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        skTransaction?.stopRecording()
+        currentProcessingText = textField
+    }
+    
+    // MARK: - SKTransaction delegates
+    
+    func transaction(_ transaction: SKTransaction!, didReceive recognition: SKRecognition!) {
+        let topRecognitionText = recognition.text
+        
+        if let currentProcessingElement = currentProcessingText {
+            if currentProcessingElement is UITextField {
+                let textField = currentProcessingElement as! UITextField
+                textField.text = topRecognitionText
+            } else if currentProcessingElement is UITextView {
+                let textView = currentProcessingElement as! UITextView
+                textView.text = topRecognitionText
+            }
+        }
+    }
 }
 
